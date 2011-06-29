ad89785aff247c34a8f1e58dba12670c461508c1
Luper Rouch <luper.rouch@gmail.com>
2011-06-30 01:28:53 +0200

    made scripts to install a virtualenv and run the sample site under gunicorn

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+virtualenv --no-site-packages --distribute venv
+. venv/bin/activate
+pip install gunicorn django>=1.3 supervisor
---
+++ run-gunicorn.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+. venv/bin/activate
+kill `cat var/run/gunicorn.pid`
+cd sample_site
+gunicorn_django --bind unix:/tmp/leechy.sock --daemon --pid ../var/run/gunicorn.pid
