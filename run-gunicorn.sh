9691951074cf0ea8933145c31b14325054a1af27
Luper Rouch <luper.rouch@gmail.com>
2011-06-30 09:07:32 +0200

    added a sleep after killing gunicorn

--- run-gunicorn.sh
+++ run-gunicorn.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 . venv/bin/activate
 kill `cat var/run/gunicorn.pid`
+sleep 2
 cd sample_site
 gunicorn_django --bind unix:/tmp/leechy.sock --daemon --pid ../var/run/gunicorn.pid --worker-class gevent --log-file ../var/log/leechy.log
