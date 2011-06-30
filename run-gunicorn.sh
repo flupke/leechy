b7d099d505e641b133684a91b66de40724780dce
Luper Rouch <luper.rouch@gmail.com>
2011-06-30 02:02:07 +0200

    using gevent worker

--- run-gunicorn.sh
+++ run-gunicorn.sh
@@ -2,4 +2,4 @@
 . venv/bin/activate
 kill `cat var/run/gunicorn.pid`
 cd sample_site
-gunicorn_django --bind unix:/tmp/leechy.sock --daemon --pid ../var/run/gunicorn.pid
+gunicorn_django --bind unix:/tmp/leechy.sock --daemon --pid ../var/run/gunicorn.pid --worker-class gevent
